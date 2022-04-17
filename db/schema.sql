edd890e5fc5de028d35cc35e7f4ce328967a3256
bcorporal <bcorporal@gmail.com>
2022-04-17 14:03:25 -0400

    schema complete with tables

--- db/schema.sql
+++ db/schema.sql
@@ -7,47 +7,32 @@ CREATE DATABASE employeetracker_db;
 -- Use employeetracker_db --
 USE employeetracker_db;
 
--- Creates the table "department" within inventory_db --
-CREATE TABLE department
+-- Creates the table "department" within employeetracker_db --
+CREATE TABLE department (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL
+);
 
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY_KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 
 
-
-
-
-
-
-
-
-
--- As the image illustrates, your schema should contain the following three tables:
-
--- department
-
--- id: INT PRIMARY KEY
-
--- name: VARCHAR(30) to hold department name
-
--- role
-
--- id: INT PRIMARY KEY
-
--- title: VARCHAR(30) to hold role title
-
--- salary: DECIMAL to hold role salary
-
--- department_id: INT to hold reference to department role belongs to
-
--- employee
-
--- id: INT PRIMARY KEY
-
--- first_name: VARCHAR(30) to hold employee first name
-
--- last_name: VARCHAR(30) to hold employee last name
-
--- role_id: INT to hold reference to employee role
-
--- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
 
