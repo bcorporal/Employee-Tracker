dec9cc01b5dba739245c7d304e6e209f3c647384
bcorporal <bcorporal@gmail.com>
2022-04-17 15:01:01 -0400

    modified seeds file to add values for department role and employees

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (name)
+VALUES ("Finance"),
+       ("Operations"),
+       ("Athletic"),
+       ("Player"),;
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Account Evecutive", 90000, 1),
+       ("SR. Account Executive", 130000, 1),
+       ("Head Coach", 8000000, 2),
+       ("Assistant Coach", 2000000, 2),
+       ("General Manager", 1500000, 2),
+       ("Assistant General Manager", 1000000, 2)
+       ("Trainer" 240000, 3),
+       ("Center" 1400000, 4),
+       ("Point Guard" 2000000, 4),
+       ("Power Forward" 1500000, 4),
+       ("Shooting Guard" 1800000, 4),;
+
+
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+       ("Robert", "Deniro", 1),
+       ("Dave", "Joerger", 2),
+       ("Doc", "Rivers", 2),
+       ("Elton", "Brand", 2),
+       ("Ned", "Chohen", 2),
+       ("Kevin", "Johnson", 3),
+       ("Joel", "Embiid", 4),
+       ("James", "Harden", 4),
+       ("Tobias", "Harris", ),
+       ("Tyrese", "Maxey", 4),;
